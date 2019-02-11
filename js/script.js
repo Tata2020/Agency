5cdd30cd9d8c8211f28fe3435fee186e20cbb480
Tata2020 <tata.khokhlova@gmail.com>
2019-02-11 21:55:39 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+$('.carousel__wrapper').mycarousel({
+    imgWidth: 280,
+    elSlider: 3,
+    textColor: '#448834',
+    borderColor: '#CCCCFF',
+    backgrArrow: '#CCCCFF',
+    speedSlider: 500
+    });
+
+});
